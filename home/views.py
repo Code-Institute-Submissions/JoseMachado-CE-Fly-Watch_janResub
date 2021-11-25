70b839d74d95403c37343c677a0d33877802a2af
JoseMachado-CE <jose@letsgetchecked.com>
2021-11-25 11:30:17 +0000

    Added home products function to home view

--- home/views.py
+++ home/views.py
@@ -7,3 +7,21 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def home_products(request):
+    """3 products from home page"""
+    homeProducts = []
+    product_one = '/media/rolex_daytona.jpeg'
+    product_two = '/media/rolex_skydweller.jpeg'
+    product_three = '/media/rolex_gmtmasterpepsi.jpeg'
+
+    homeProducts.append(product_one,product_two,product_three)
+
+    ThreeProducts = homeProducts.objects.all()
+
+    template = 'home/index.html'
+    context = {
+        'ThreeProducts': ThreeProducts,
+    }
+    return render(request, template, context)
