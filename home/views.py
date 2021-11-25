82639a966d9420310391093f0624b8b9ae23f89c
JoseMachado-CE <jose@letsgetchecked.com>
2021-11-25 13:45:36 +0000

    Removed some of code from index home function

--- home/views.py
+++ home/views.py
@@ -6,19 +6,19 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page """
 
-    homeProducts = []
-    product_one = '/media/rolex_daytona.jpeg'
-    product_two = '/media/rolex_daytona.jpeg'
-    product_three = '/media/rolex_daytona.jpeg'
+    # homeProducts = []
+    # product_one = '/media/rolex_daytona.jpeg'
+    # product_two = '/media/rolex_daytona.jpeg'
+    # product_three = '/media/rolex_daytona.jpeg'
 
-    homeProducts.append(product_one)
-    homeProducts.append(product_two)
-    homeProducts.append(product_three)
+    # homeProducts.append(product_one)
+    # homeProducts.append(product_two)
+    # homeProducts.append(product_three)
 
     template = 'home/index.html'
-    context = {
+    # context = {
         
-        'homeProducts': homeProducts,
-    }
+    #     'homeProducts': homeProducts,
+    # }
 
-    return render(request, context, template)
+    return render(request, template)
