1959f80e059b48e7a58d0c9c1626d2eea561ab03
JoseMachado-CE <jose@letsgetchecked.com>
2021-11-25 11:42:54 +0000

    Fixed typo in index template code

--- home/views.py
+++ home/views.py
@@ -9,19 +9,3 @@ def index(request):
     return render(request, 'home/index.html')
 
 
-def home_products(request):
-    """3 products from home page"""
-    homeProducts = []
-    product_one = '/media/rolex_daytona.jpeg'
-    product_two = '/media/rolex_skydweller.jpeg'
-    product_three = '/media/rolex_gmtmasterpepsi.jpeg'
-
-    homeProducts.append(product_one,product_two,product_three)
-
-    ThreeProducts = homeProducts.objects.all()
-
-    template = 'home/index.html'
-    context = {
-        'ThreeProducts': ThreeProducts,
-    }
-    return render(request, template, context)
